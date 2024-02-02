9b6988ea60e66a6868bc937f5485a2b66497b487
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-02-02 17:06:03 -0600

    Changed were made.

--- Subtraction Cancellation Exercise 2/ContentView.swift
+++ Subtraction Cancellation Exercise 2/ContentView.swift
@@ -6,9 +6,7 @@ struct ContentView: View {
     @State private var resultS2: Float = 0.0
     @State private var resultS3: Float = 0.0
     @State private var errorMessage: String? = nil
-    
-    
-    //Displays the calculations for S^(1), S^(2), and S^(3)
+    @State private var iterations: [Int] = []
     
     var body: some View {
         VStack {
@@ -22,21 +20,15 @@ struct ContentView: View {
                 .font(.headline)
                 .fontWeight(.regular)
             
-            
             //Displays the calculations for S^(1), S^(2), and S^(3)
-            
             TextField("Enter N", text: $inputN)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
             Button("Calculate") {
-                
                 // Check if input N is a valid integer. It will display an error message if it is not.
-                
                 guard let n = Int(inputN) else {
-                    
                     // Reset result variables and display error message
-                    
                     resultS1 = 0.0
                     resultS2 = 0.0
                     resultS3 = 0.0
@@ -45,26 +37,22 @@ struct ContentView: View {
                 }
                 
                 // This calls the function calculatesS for the results of s1, s2, s3
-                
                 let (s1, s2, s3) = calculateS(for: Float(n))
                 resultS1 = s1
                 resultS2 = s2
                 resultS3 = s3
                 
                 // Reset error message
-                
                 errorMessage = nil
             }
             .padding()
             
             // Displays the results of S^(1), S^(2), and S^(3)
-            
             Text("S^(1) = \(resultS1)")
             Text("S^(2) = \(resultS2)")
             Text("S^(3) = \(resultS3)")
             
             // Display error message if present
-            
             if let errorMessage = errorMessage {
                 Text(errorMessage)
                     .foregroundColor(.red)
@@ -75,16 +63,24 @@ struct ContentView: View {
             Text("Problem 2b - Log-Log Plot")
                 .underline(true, color: .black)
                 .font(.system(size: 20))
-                .padding(.top, 20)
+                .padding()
             
             Text("We assume S^(3) is correct. We generate a Log-Log plot based off 1,000,000 iterations.")
                 .font(.headline)
-                .fontWeight(.regular)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-    }
-}
+                .fontWeight(.regular)                    }
+                }
+            }
+      
+            
+            
+
+
+
+
+    
+
+
+
 
 
 
